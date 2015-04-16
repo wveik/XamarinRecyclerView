e772608eada758a148945657540a9df266952462
wveik <kataev_06@list.ru>
2015-04-16 13:06:11 +0300

    Первый commit

---
+++ XamarinRecyclerView/Domain/Email.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinRecyclerView.Domain {
+    public class Email {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string Time { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
---
+++ XamarinRecyclerView/MainActivity.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using XamarinRecyclerView.Domain;
+using System.Collections.Generic;
+
+namespace XamarinRecyclerView {
+    [Activity(Label = "XamarinRecyclerView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        private RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager mLayoutManager;
+        private RecyclerView.Adapter mAdapter;
+        private List<Email> mEmails;
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            mEmails = new List<Email>();
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+            mEmails.Add(new Email() { Name = "tom", Subject = "Wanna hang out?", Message = "I'll be around tomorrow!!" });
+
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+            //Create out layout manager
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new RecyclerAdapter(mEmails);
+            mRecyclerView.SetAdapter(mAdapter);
+
+        }
+    }
+
+    public class RecyclerAdapter : RecyclerView.Adapter {
+        private List<Email> mEmails;
+
+        public RecyclerAdapter(List<Email> emails) {
+            mEmails = emails;
+        }
+
+        public class MyView : RecyclerView.ViewHolder {
+            public View mMainView { get; set; }
+            public TextView mName { get; set; }
+            public TextView mSubject { get; set; }
+            public TextView mMessage { get; set; }
+
+            public MyView(View view)
+                : base(view) {
+                mMainView = view;
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+            View row = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.row, parent, false);
+
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.txtName);
+            TextView txtSubject = row.FindViewById<TextView>(Resource.Id.txtSubject);
+            TextView txtMessage = row.FindViewById<TextView>(Resource.Id.txtMessage);
+
+            MyView view = new MyView(row) { mName = txtName, mSubject = txtSubject, mMessage = txtMessage };
+            return view;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyView myHolder = holder as MyView;
+            myHolder.mName.Text = mEmails[position].Name;
+            myHolder.mSubject.Text = mEmails[position].Subject;
+            myHolder.mMessage.Text = mEmails[position].Message;
+        }
+
+        public override int ItemCount {
+            get { return mEmails.Count; }
+        }
+    }
+}
